9fefa28c538212489cc860f0ad6e4b1dc1ec680d
A  S  L  A  N  B  E  K <learneraslanbek1220@gmail.com>
2023-01-17 09:32:25 +0300

    comment

--- FastFoods/Program.cs
+++ FastFoods/Program.cs
@@ -9,7 +9,7 @@ namespace FastFoods
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application.I am Aslan
         /// </summary>
         [STAThread]
         static void Main()
